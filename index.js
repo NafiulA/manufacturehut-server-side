12b1956d6d57eb72081b57dfcdfcfbca1408cd19
Nafiul Alam <nafiulalam30@gmail.com>
2022-05-23 17:09:50 +0600

    user database updating with jwt

--- index.js
+++ index.js
@@ -18,11 +18,25 @@ async function run() {
 
         const database = client.db("manufacturehut");
         const productCollection = database.collection("products");
+        const userCollection = database.collection("users");
 
         app.get("/products", async (req, res) => {
             const query = {}
             const products = await productCollection.find(query).toArray();
             res.send(products);
+        });
+
+        app.put("/user/:email", async (req, res) => {
+            const email = req.params.email;
+            const body = req.body;
+            const query = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: body,
+            };
+            const result = await userCollection.updateOne(query, updateDoc, options);
+            const accessToken = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+            res.send({ result, accessToken });
         })
     }
     finally {
