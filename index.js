0b15a00fca94ff752de794a71b7cd61b0c95e2f7
Nafiul Alam <nafiulalam30@gmail.com>
2022-05-24 13:32:53 +0600

    make and verify admin

--- index.js
+++ index.js
@@ -12,6 +12,22 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9h6ig.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+};
+
 async function run() {
     try {
         await client.connect();
@@ -20,6 +36,17 @@ async function run() {
         const productCollection = database.collection("products");
         const userCollection = database.collection("users");
         const orderCollection = database.collection("orders");
+
+        async function verifyAdmin(req, res, next) {
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === "admin") {
+                next();
+            }
+            else {
+                res.status(403).send({ message: "Forbidden Access" });
+            }
+        }
 
         app.get("/products", async (req, res) => {
             const query = {}
@@ -48,13 +75,13 @@ async function run() {
         });
 
 
-        app.post("/order", async (req, res) => {
+        app.post("/order", verifyJWT, async (req, res) => {
             const body = req.body;
             const result = await orderCollection.insertOne(body);
             res.send(result);
         });
 
-        app.put("/updateQuantity/:id", async (req, res) => {
+        app.put("/updateQuantity/:id", verifyJWT, async (req, res) => {
             const id = req.params.id;
             const quantity = req.query.quantity;
             const filter = { _id: ObjectId(id) }
@@ -64,6 +91,32 @@ async function run() {
                 },
             }
             const result = await productCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        });
+
+        app.get("/user", verifyJWT, verifyAdmin, async (req, res) => {
+            const query = {};
+            const result = await userCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.get("/admin/:email", async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const isAdmin = user.role === "admin";
+            res.send({ admin: isAdmin });
+        })
+
+        app.put("/user/admin/:email", verifyJWT, verifyAdmin, async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const updateDoc = {
+                $set: {
+                    role: "admin"
+                }
+            };
+            const result = await userCollection.updateOne(query, updateDoc);
             res.send(result);
         })
     }
